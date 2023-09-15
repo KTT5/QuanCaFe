c60e834d8ff52abdeb1069cc138158bb0a580f7d
PhamDangKhoa <khoaphd2201@gmail.com>
2023-09-15 11:15:24 +0700

    Giao dien chinh

--- QLQuanCaFe/QLQuanCaFe/Program.cs
+++ QLQuanCaFe/QLQuanCaFe/Program.cs
@@ -16,7 +16,7 @@ namespace QLQuanCaFe
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new TrangChu());
         }
     }
 }
--- QLQuanCaFe/QLQuanCaFe/Them.cs
+++ QLQuanCaFe/QLQuanCaFe/Them.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QLQuanCaFe
 {
-    public partial class Form1 : Form
+    public partial class Them : Form
     {
-        public Form1()
+        public Them()
         {
             InitializeComponent();
         }
