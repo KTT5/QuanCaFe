132646455bb5e6090ee8189ebe265a9271b48938
LeTin27 <90137584+LeTin27@users.noreply.github.com>
2023-10-27 16:59:22 +0700

    Thống Kê

---
+++ QLQuanCaFe/QLQuanCaFe/GUI/ThongKe.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace QLQuanCaFe.GUI
+{
+    partial class ThongKe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtgvBill = new System.Windows.Forms.DataGridView();
+            this.btnThongKe = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvBill)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(57, 26);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.Value = new System.DateTime(2023, 10, 27, 15, 24, 5, 0);
+            // 
+            // dtgvBill
+            // 
+            this.dtgvBill.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvBill.Location = new System.Drawing.Point(27, 78);
+            this.dtgvBill.Name = "dtgvBill";
+            this.dtgvBill.Size = new System.Drawing.Size(511, 239);
+            this.dtgvBill.TabIndex = 2;
+            // 
+            // btnThongKe
+            // 
+            this.btnThongKe.Location = new System.Drawing.Point(339, 26);
+            this.btnThongKe.Name = "btnThongKe";
+            this.btnThongKe.Size = new System.Drawing.Size(75, 23);
+            this.btnThongKe.TabIndex = 3;
+            this.btnThongKe.Text = "Thống Kê";
+            this.btnThongKe.UseVisualStyleBackColor = true;
+            this.btnThongKe.Click += new System.EventHandler(this.btnThongKe_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(420, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Lưu File TxT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // ThongKe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(580, 356);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnThongKe);
+            this.Controls.Add(this.dtgvBill);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "ThongKe";
+            this.Text = "ThongKe";
+            this.Load += new System.EventHandler(this.ThongKe_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvBill)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DataGridView dtgvBill;
+        private System.Windows.Forms.Button btnThongKe;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ QLQuanCaFe/QLQuanCaFe/GUI/ThongKe.cs
@@ -0,0 +1,120 @@
+﻿using DAL_BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace QLQuanCaFe.GUI
+{
+    public partial class ThongKe : Form
+    {
+        QLCFDataContext data = new QLCFDataContext();
+        public ThongKe()
+        {
+            InitializeComponent();
+        }
+        void LoadListBillByDate(DateTime checkIn)
+        {
+            var query = from donHang in data.DonHangs
+                        where donHang.NgayDatHang == checkIn
+                        select new
+                        {
+                            DonHang = donHang,
+                            ChiTietDonHangs = data.ChiTietDonHangs.Where(ctdh => ctdh.MaDonHang == donHang.MaDonHang).ToList()
+                        };
+
+            foreach (var item in query)
+            {
+                DonHang donHang = item.DonHang;
+                List<ChiTietDonHang> ctdh = item.ChiTietDonHangs;
+                foreach (var ch in ctdh)
+                {
+                    dtgvBill.Rows.Add(donHang.MaDonHang, donHang.NgayDatHang, donHang.NhanVien.TenNhanVien, ch.SoLuong, ch.SanPham.TenSanPham, ch.Topping.Ten);
+                }
+
+            }
+
+        }
+        void setDataGridView()
+        {
+            dtgvBill.Columns.Add("MaDonHang", "Mã Đơn Hàng");
+            dtgvBill.Columns.Add("NgayDatHang", "Ngày Đặt Hàng");
+            dtgvBill.Columns.Add("TenNhanVien", "Tên Nhân Viên");
+            dtgvBill.Columns.Add("SoLuong", "Số Lượng");
+            dtgvBill.Columns.Add("TenSanPham", "Sản Phẩm");
+            dtgvBill.Columns.Add("Ten", "Topping");
+        }
+
+        private void btnThongKe_Click(object sender, EventArgs e)
+        {
+            dtgvBill.Rows.Clear();
+            LoadListBillByDate(dateTimePicker1.Value);
+        }
+
+        private void ThongKe_Load(object sender, EventArgs e)
+        {
+            setDataGridView();
+            //LoadListBillByDate(dateTimePicker1.Value);
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // Tạo một đối tượng SaveFileDialog
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+
+            // Thiết lập tiêu đề của hộp thoại
+            saveFileDialog1.Title = "Lưu tệp";
+
+            // Thiết lập bộ lọc tệp cho hộp thoại (ví dụ: chỉ cho phép lưu tệp văn bản)
+            saveFileDialog1.Filter = "Tệp Văn bản|*.txt|Tất cả các tệp|*.*";
+
+            // Thiết lập thư mục mặc định khi hộp thoại mở lên (tùy chọn)
+            saveFileDialog1.InitialDirectory = @"C:\";
+
+            // Hiển thị hộp thoại SaveFileDialog và kiểm tra xem người dùng đã chọn một vị trí và tên tệp chưa
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Lấy đường dẫn đầy đủ của tệp được chọn
+                string filePath = saveFileDialog1.FileName;
+
+                // Tạo một chuỗi để lưu dữ liệu từ DataGridView
+                string dataToSave = "";
+
+                // Lưu tên cột đầu tiên vào chuỗi (header)
+                foreach (DataGridViewColumn column in dtgvBill.Columns)
+                {
+                    dataToSave += column.HeaderText.PadRight(20) + "\t"; // Sử dụng PadRight để đảm bảo độ rộng là 20 ký tự
+                }
+                dataToSave += Environment.NewLine; // Xuống dòng để tách dòng
+
+                // Duyệt qua các dòng trong DataGridView và lưu dữ liệu vào chuỗi
+                foreach (DataGridViewRow row in dtgvBill.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        if (cell.Value != null)
+                        {
+                            dataToSave += cell.Value.ToString().PadRight(20) + "\t"; // Sử dụng PadRight để đảm bảo độ rộng là 20 ký tự
+                        }
+                    }
+                    dataToSave += Environment.NewLine; // Xuống dòng để tách dòng
+                }
+                // Tạo dòng "Thống kê Hóa đơn" và đặt nó vào giữa
+                string header = "Thống kê Hóa đơn".PadLeft(dataToSave.Length / 2 + "Thống kê Hóa đơn".Length / 2);
+                dataToSave = header + Environment.NewLine + dataToSave;
+
+                // Lưu chuỗi dữ liệu vào tệp văn bản
+                File.WriteAllText(filePath, dataToSave);
+
+                // Thông báo lưu thành công
+                MessageBox.Show("Dữ liệu đã được lưu tại: " + filePath, "Lưu tệp", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
